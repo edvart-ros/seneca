85ecbf65472be90a22b3ecec12cdcff6631b1896
edvart-ros <94528774+edvart-ros@users.noreply.github.com>
2021-11-17 15:27:15 +0100

    Update figur_server.py

--- echopkg/src/actions/figur_server.py
+++ echopkg/src/actions/figur_server.py
@@ -19,36 +19,39 @@ class Bil(object):
   
   def __init__(self):
     self._as = actionlib.SimpleActionServer('/figur_action', FigurAction, self.execute_callback, auto_start = False) #starter action server
-    #self.sub = rospy.Subscriber('/echo', Float32, self.sub_cb, queue_size = 1)                          #subscriber til echo
+    self.sub = rospy.Subscriber('/echo', Float32, self.sub_cb, queue_size = 1)                          #subscriber til echo
     self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)                                      #lager publisher til cmd_vel
     self._feedback = FigurFeedback()                                                                    #dewfinerer feedback-object
     self._result = FigurResult()                                                                       #definerer resultat-object
     self._as.start()                                                                                    #starter server
-    #self.ctrl_c = False
+    self.ctrl_c = False
     self.rospy = rospy
     self.avstand = Float32()
     self.var = Twist()
     self.success = False
     self.cancelled = 3
     
+    self.tik = time.time()
+    self.tok = time.time()
     
     
-#############################  HJELPEFUNKSJONER, TOPIC CALLBACKS
-#############################
+    
+##########################################################
+##########################################################
 
 
   
   def sub_cb(self, msg):
     dist = msg.data
     if dist < 15 and self.cancelled == 0:
-      self.rospy.loginfo("goal preempted")
+      self.rospy.loginfo("action aborted, waiting for new goal")
+      self._result.tid = self.tok-self.tik
       self._as.set_aborted(self._result)
       self.success = False
       self.stopp()
       self.cancelled = 1
       return self.cancelled
       
-    
     
   def stopp(self):
     self.var.linear.x = 0
@@ -61,6 +64,7 @@ class Bil(object):
   
     self.stopp()
     time.sleep(0.1)
+    
     
     for i in range(4):
       
@@ -85,25 +89,22 @@ class Bil(object):
       self.pub.publish(self.var)
     
     
-################################ HOVEDFUNKSJON
-################################
+#############################################################
+#############################################################
 
     
   def execute_callback(self, goal):
     
-    self.rospy.loginfo("action server called with argument:" + str(goal))
+    self.rospy.loginfo("goal recieved:" + str(goal))
     
-    self.sub = rospy.Subscriber('/echo', Float32, self.sub_cb, queue_size = 1)
     self.cancelled = 0
-
-    
     
     i = 0
-    tik = time.time()
+    self.tik = time.time()
     self.stopp()
 
     
-    if goal.figur == "sirkel":
+    if goal.figur == "sirkel_left":
       self.success = True
       
       while i < goal.reps:
@@ -112,24 +113,37 @@ class Bil(object):
         self._feedback.current_rep = i+1
         self._as.publish_feedback(self._feedback)
         
-        self.var.linear.x = .8
+        self.var.linear.x = .0
         self.var.angular.z = 0.8
         self.pub.publish(self.var)
         for i in range(6):
           time.sleep(0.517)          
-          
         
         i += 1
       
-      tok = time.time()
       
+    if goal.figur == "sirkel_right":
+      self.success = True
+      
+      while i < goal.reps:
+      
+      
+        self._feedback.current_rep = i+1
+        self._as.publish_feedback(self._feedback)
+        
+        self.var.linear.x = 0.0
+        self.var.angular.z = -0.8
+        self.pub.publish(self.var)
+        for i in range(6):
+          time.sleep(0.517)          
+        
+        i += 1
       
       
     #om firkant goal
     
     elif goal.figur == "firkant":
       
-      tik = time.time()
       
       while i < goal.reps:
       
@@ -140,27 +154,33 @@ class Bil(object):
 
         i += 1
       
-      
-      tok = time.time()  #maler tid igjen
+     
     
+    elif goal.figur == "straight":
+      while True:
+        self.var.linear.x = 0.5
+        self.pub.publish(self.var)
+        
+        if self.cancelled == 1:
+          break
+    
+
     self.stopp()
-    self._result.tid = tok-tik
+    self.tok = time.time()
+    
+    self._result.tid = self.tok-self.tik
     
       
     if self.success == True:
-      rospy.loginfo('success')
+      rospy.loginfo('success, waiitng for new goal')
       self._as.set_succeeded(self._result)
       return True
       
+##########################################################
+##########################################################
 
 if __name__ == '__main__':
-  rospy.loginfo('server started')
   rospy.init_node('figur_server')
+  rospy.loginfo('waiting for goal')
   Bil()
   rospy.spin()
-
-      
-      
-      
-    
-    
