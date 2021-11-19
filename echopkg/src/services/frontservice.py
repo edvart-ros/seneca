f7f11d642805e89873a69400f13b4fb24e3f7760
edvart-ros <94528774+edvart-ros@users.noreply.github.com>
2021-11-19 10:25:04 +0100

    Update frontservice.py

--- echopkg/src/services/frontservice.py
+++ echopkg/src/services/frontservice.py
@@ -3,34 +3,34 @@
 import rospy
 from mDev import *
 from std_srvs.srv import Trigger, TriggerRequest, TriggerResponse
-mdev = mDEV()
 from std_msgs.msg import Float32
 
 import time
 
 avstand = Float32()
-mdev = mDEV()
-
 
 
 def callback(request):
     response = TriggerResponse()
     mdev.setServo('2', 90)
-    avstand = rospy.wait_for_message("/cmd_vel", Float32)
     time.sleep(0.7)
+    avstand = rospy.wait_for_message("/cmd_vel", Float32)
+
 
     if avstand < 1:
         response.success = True
+        response.message = "crash"
     
     else:
         response.success = False
+        response.message = "klart"
 
     return TriggerResponse()
 
 
 
 
-rospy.init_node("subpubtingen")
+rospy.init_node("crash_front_server")
 my_service = rospy.Service("/crash_front_service", Trigger, callback)
 rospy.spin()
 
